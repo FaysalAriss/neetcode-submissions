641050c7a148214cb26af19af9068b0b35000f33
Faysal <40504311+FaysalAriss@users.noreply.github.com>
2026-08-16 23:55:05 +0100

    Add: longest-repeating-substring-with-replacement - submission-24

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-24.py
@@ -0,0 +1,22 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        charCount = defaultdict(int)
+        left = 0
+        right = left
+        maxCount = 0
+        maxLength = 0
+
+        while right < len(s):  
+            currentChar = s[right]
+            charCount[currentChar] += 1
+            maxCount = max(maxCount, charCount[currentChar])
+
+            #if invalid substring then increment left
+            if right-left+1 - maxCount > k:
+                charCount[s[left]] -= 1
+                left += 1
+
+            maxLength = max(maxLength, right-left+1)
+            right += 1
+
+        return max(maxLength,right-left)
